e8c765bdf3a2e8e2c1e807511f597daa630a50ff
Ellen Xu <33074674+ellen-xu@users.noreply.github.com>
2018-01-05 17:52:08 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+ <!DOCTYPE html>
+<html>
+
+<head>
+  <title> Impact Investing Club </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+  <script type="text/javascript" src="scripts/jquery-3.2.1.js"></script>
+  <script type="text/javascript" src="scripts/slide.js"></script>
+
+
+</head>
+
+<body id="home">
+  <div class= "wrapper">
+
+    <?php include "includes/navigation.php"; ?>
+
+  </div>
+
+ <!-- <div class="push"></div> -->
+ <div id="quote">
+   <div id="text">
+   <p id="damn-quote">"The best investment any of us can make is in the lives of others."
+   <span id="damn-quote2">- Bill and Melinda Gates </span></p>
+  </div>
+ </div>
+    <footer>
+      <p class="footer-caption1">Email us: <a id="footer-email" href="mailto:rm686@cornell.edu?Subject=Potential%20Partnership">rm686@cornell.edu</a></p>
+
+      <p class="footer-caption2">Meeting Location: Kennedy Hall Room 213, Cornell University, Ithaca, NY</p>
+
+    </footer>
+
+</body>
+
+</html>
