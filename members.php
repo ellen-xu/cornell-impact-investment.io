1b829548856038c49db5e164fa2fa311f96b3666
Ellen Xu <33074674+ellen-xu@users.noreply.github.com>
2018-01-05 18:12:38 +0000

    Create members.php

---
+++ members.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title> Impact Investing Club </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+  <script type="text/javascript" src="scripts/jquery-3.2.1.js"></script>
+  <script src="scripts/site.js" type="text/javascript"></script>
+</head>
+
+<body>
+
+  <div id="modalBack">
+
+    <div class="myModal modal">
+
+      <!-- Modal content -->
+      <div class="modal-content">
+
+        <figure>
+          <img src="images/rahul.jpg" class="circle" alt="personimg">
+        </figure>
+        <h3>Rahul Mukherjee</h3>
+        <p class="title">Chief Executive Officer</p>
+        <p class="bio">Rahul is a sophomore at Cornell University, studying Biology, Mathematics, and Economics.
+          He lives in Salt Lake City, Utah where he interned as an investment banking analyst this past summer.
+          In his free time, he enjoys playing piano, birding, and playing pick-up soccer with friends. </p>
+
+          <p><a href="mailto:rm686@cornell.edu?Subject=Impact%20Investing"><img alt="mail" class="logo" src="images/gmail.png"></a></p>
+
+        </div>
+
+      </div>
+    </div>
+
+    <div id="modalBack2">
+
+      <div class="myModal modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+
+
+          <figure>
+            <img src="images/yousef.jpg" class="circle" alt="personimg">
+          </figure>
+          <h3>Yousef Elraghy</h3>
+          <p class="title">Finance Director</p>
+          <p class="bio">Yousef is a sophomore in Cornell University's Dyson School
+            of Applied Economics and Management. He's a driven and analytical
+            individual who thrives in a collaborative environment. His
+            international exposure (living in the Middle East for 7 years),
+            has honed his cross-cultural communication skills and has allowed him
+            to become an effective leader in a diverse team. </p>
+            <p><a href="mailto:yte4@cornell.edu?Subject=Impact%20Investing"><img alt="mail" class="logo" src="images/gmail.png"></a>
+              <a target="_blank" href="https://www.linkedin.com/in/yousef-el-raghy-32a67712a/"><img alt="linkedin" class="logo" src="images/linkedin.png"></a></p>
+
+
+            </div>
+
+          </div>
+        </div>
+
+        <div id="modalBack3">
+
+          <div class="myModal modal">
+
+            <!-- Modal content -->
+            <div class="modal-content">
+
+
+              <figure>
+                <img src="images/ari.jpg" class="circle" alt="personimg">
+              </figure>
+              <h3>Arihant Jain</h3>
+              <p class="title">Chief Investing Officer</p>
+              <p class="bio">Arihant is a student in the SC Johnson College of Business at
+                Cornell University. He's majoring in Applied Economics and Management and will graduate in
+                three years. He will complete a Masters in Behavioral Finance in my fourth year.
+                He desires to pursue his passion for finance, the startup world, venture capital,
+                and business management to create value and to make an impact.</p>
+                <p><a href="mailto:aj387@cornell.edu?Subject=Impact%20Investing"><img alt="mail" class="logo" src="images/gmail.png"></a>
+                  <a target="_blank" href="https://www.linkedin.com/in/arihantjain4/"><img alt="linkedin" class="logo" src="images/linkedin.png"></a></p>
+                </div>
+
+              </div>
+            </div>
+            <div id="modalBack4">
+              <div class="myModal modal">
+                <!-- Modal content -->
+                <div class="modal-content">
+                  <figure>
+                    <img src="images/ellen.jpg" class="circle" alt="personimg">
+                  </figure>
+                  <h3>Ellen Xu</h3>
+                  <p class="title">Chief of Communication Outreach</p>
+                  <p class="bio">Ellen is a sophomore studying Information Science.
+                    This past summer she worked at HNA Group, a Chinese conglomerate
+                    involved in the aviation,and xtLytics,a healthcare analytics startup.
+                    In her free time, she enjoys exploring photography, astronomy, attending hackathons, and swimming. </p>
+                    <p><a href="mailto:ex24@cornell.edu?Subject=Impact%20Investing"><img alt="mail" class="logo" src="images/gmail.png"></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/ellen-xu-947947129/"><img alt = "linkedin" class="logo" src="images/linkedin.png"></a></p>
+                </div>
+
+                </div>
+              </div>
+
+            <div id="modalBack5">
+              <div class="myModal modal">
+                <!-- Modal content -->
+                <div class="modal-content">
+                  <figure>
+                    <img src="images/mazen.jpg" class="circle" alt="personimg">
+                  </figure>
+                  <h3>Mazen Raafat Ibrahim</h3>
+                  <p class="title">Secretary</p>
+                  <p class="bio">Mazen is a junior from Alexandria, Egypt studying in Computer Science and minoring in Business.
+                    This past summer, Mazen worked at Facebook doing software engineering in New York City. </p>
+                    <p><a href="mailto:mri22@cornell.edu?Subject=Impact%20Investing"><img alt="mail" class="logo" src="images/gmail.png"></a></p>
+                </div>
+                </div>
+              </div>
+
+              <div id="modalBack6">
+
+                <div class="myModal modal">
+
+                  <!-- Modal content -->
+                  <div class="modal-content">
+                    <figure>
+                      <img src="images/samir.jpg" class="circle" alt="personimg">
+                    </figure>
+                    <h3>Samir Salih</h3>
+                    <p class="title">Chief Operations Officer</p>
+
+                    <p class="bio">Samir is an undergraduate student at Cornell University interested
+                      in comparative government and International law. He is interested
+                      in opportunities that will challenge his writing and analytical skills
+                      in fields such as Diplomacy, Law, Government, and Finance.</p>
+                      <p><a href="mailto:sms599@cornell.edu?Subject=Impact%20Investing"><img alt="mail" class="logo" src="images/gmail.png"></a>
+                        <a target="_blank" href="https://www.linkedin.com/in/samir-salih/"><img alt = "linkedin" class="logo" src="images/linkedin.png"></a></p>
+                      </div>
+
+                    </div>
+                  </div>
+
+
+
+                  <?php include "includes/navigation.php"; ?>
+
+
+
+                  <div id="missionContain">
+                    <h2>Our Mission</h2>
+                    <p>The mission of CII shall be to educate members on sustainable investment models, and to work
+                      with impact investment firms, in order to provide research support to help challenge market and
+                      social issues. Investments shall be focused on intentional resolution or alleviation of issues in
+                      different sectors with ethical objectives. Any funds raised by CII shall be used to make impact
+                      investments.
+                    </p>
+                  </div>
+                  <h2>Our Team</h2>
+                  <div class="teamContainer">
+                    <div class="row">
+                      <div class="teamMember first" id="TeamMember1">
+                        <figure>
+                          <img src="images/rahul.jpg" class="circle" alt="personimg">
+                        </figure>
+                        <h3>Rahul Mukherjee</h3>
+                        <p class="title">Chief Executive Officer</p>
+
+                        <p id="first">Meet Rahul &rarr; </p>
+
+
+                      </div>
+
+
+
+                      <div class="teamMember second">
+                        <figure>
+                          <img src="images/yousef.jpg" class="circle" alt="personimg">
+                        </figure>
+                        <h3>Yousef Elraghy</h3>
+                        <p class="title">Finance Director</p>
+
+                        <p id="second">Meet Yousef &rarr; </p>
+                      </div>
+
+                      <div class="teamMember third">
+                        <figure>
+                          <img src="images/ari.jpg" class="circle" alt="personimg">
+                        </figure>
+                        <h3>Ari Jain</h3>
+                        <p class="title">Chief Investing Officer</p>
+
+                        <p id="third">Meet Ari &rarr; </p>
+
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="teamMember fourth">
+                        <figure>
+                          <img src="images/ellen.jpg" class="circle" alt="personimg">
+                        </figure>
+                        <h3>Ellen Xu</h3>
+                        <p class="title">Chief of Communication Outreach</p>
+                        <p id="fourth">Ellen &rarr; </p>
+                      </div>
+
+                      <div class="teamMember fifth">
+                        <figure>
+                          <img src="images/mazen.jpg" class="circle" alt="personimg">
+                        </figure>
+                        <h3>Mazen Raafat Ibrahim</h3>
+                        <p class="title">Secretary</p>
+                        <p id="fifth">Meet Mazen &rarr; </p>
+                      </div>
+
+                      <div class="teamMember sixth">
+                        <figure>
+                          <img src="images/samir.jpg" class="circle" alt="personimg">
+                        </figure>
+                        <h3>Samir Salih</h3>
+                        <p class="title">Chief Operations Officer</p>
+                        <p id="sixth">Meet Samir &rarr; </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <?php include "includes/footer.php"; ?>
+                </body>
+
+                </html>
