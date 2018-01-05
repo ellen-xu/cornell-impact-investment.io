2e274e8e2d2cd6a5bb913a73b2b5e0f198f00cc1
Ellen Xu <33074674+ellen-xu@users.noreply.github.com>
2018-01-05 18:13:55 +0000

    Create about.php

---
+++ about.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <title> Impact Investing Club </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script type="text/javascript" src="scripts/jquery-3.2.1.js"></script>
+  <script type="text/javascript" src="scripts/site.js"></script>
+  <script type="text/javascript" src="scripts/animateAbout.js"></script>
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+</head>
+<body>
+  <?php include "includes/navigation.php"; ?>
+  <div id="wrapper">
+  <div id="containEd">
+  
+    <div class="impactinvestinginfo">
+      <div id= "aboutimpact">
+        <div class="sepSection" id="who">
+
+          <h1>Who Are We?</h1>
+          <p> "Given our global political climate, it has become more important
+            now than ever before to understand and take responsibility for the
+            implications of our actions as citizens of the world. I strongly
+            believe that impact investing allows people the opportunity to
+            do just that, supporting ventures that create paths toward a
+            healthier planet. Impact investing though projected to be the
+            upcoming trend within the world of finance, really hasn’t reached
+            the mainstream yet. Thus our purpose of founding CII, one of the
+            only impact investing undergraduate clubs in the country, was to
+            take on a pioneering role and allow more people the opportunity
+            to learn and get involved in impact investing. In this way, our
+            hope for CII is to not only bring in people with prior finance
+            experience, but to expand the scope of this niche to people of
+            different backgrounds and skillsets." </p>
+            <p><i>- Rahul Mukherjee, Chief Executive Officer</i></p>
+          </div>
+          <!-- <div id="definition">
+          <h2 id="deftitle">Impact Investment</h2>
+          <p id="defpronounce">[im-pakt in-vest-muh nt]</p>
+          <p>NOUN: Impact investing is investing that aims to generate specific beneficial social
+          or environmental effects in addition to financial gain.</p>
+        </div> -->
+        <div class="sepSection" id="what">
+
+        <h1>What is Impact Investing?</h1>
+        <div class="centEd">
+          <p>Impact investment is a type of investment made from companies, organizations, and funds with the purpose of
+            generating social and environmental impact as well as a financial return. Impact investments are
+            commonly made in both emerging and developed markets. These investments have a range of returns from below
+            market to market rate, depending on investors' strategic goals. </p>
+            <div class="impactIMG">
+            <img class="imgwidth" alt= "aboutimpact" src="images/aboutimpact.jpg">
+          </div>
+
+        </div>
+        <div class="sepSection" id="why">
+
+            <div id= "whyimpact">
+              <h1>Why Impact Investing?</h1>
+              <p>Impact investing is innovative and challenges the long-held views that social and environmental issues
+                should be only addressed by philanthropic donations and that market investments should exclusively
+                concentrate on achieving financial returns.</p>
+
+                <p>The impact investing market offers viable and a diversity of opportunities for
+                  investors to advance social and environmental solutions through investments
+                  which also produce financial returns.</p>
+                  <div class="impactIMG">
+                  <img class="imgwidth" alt= "whyimpact" src="images/whyimpact.jpg">
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="sepSection" id="adp">
+
+            <div class= "analystdevinfo">
+              <h1>Analyst Development Program</h1>
+              <h2>About</h2>
+              <p>The Analyst Development Program (ADP) is a 4-week long program,
+                with four 1 hour long lectures and 2 weekend workshops that
+                will give you real applicable knowledge and effective expertise.
+                By the end of the ADP, you will be able to construct comprehensive
+                Investment Proposal Decks,valuate companies, and present your ideas
+                in a coherent and applicable manner.</p>
+                <div id="calender">
+                  <div id="calender-left">
+                    <h2>Lectures and Workshops</h2>
+                    <ul>
+                      <li class="calender-li"><span class="bold">Week 1, October 24nd:</span> Accounting and Financial Terms</li>
+                      <li class="calender-li"><span class="bold">Week 2, October 31st:</span> How to Market Research</li>
+                      <li class="calender-li"><span class="bold">Week 2, November 4th:</span> (Saturday) Investment-Presentation Workshop </li>
+                      <li class="calender-li"><span class="bold">Week 3, November 7nd:</span> Valuations Part 1</li>
+                      <li class="calender-li"><span class="bold">Week 3, November 11th:</span> (Saturday) Valuations Workshop Part 1</li>
+                      <li class="calender-li"><span class="bold">Week 4, November 14th:</span> Valuations Part 2</li>
+                      <li class="calender-li"><span class="bold">Week 4, November 18th:</span> (Saturday) DCF Workshop</li>
+                    </ul>
+                  </div>
+                  <div id="calender-right">
+                    <h2>Case Study</h2>
+                    <ul>
+                      <li class="calender-li"><span class="bold">Week 4, November 18th:</span> Impact Investment Case Preparation</li>
+                      <li class="calender-li"><span class="bold">Final Week, December 4th:</span> Presentation</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+
+            <div class="sepSection" id="recruit">
+
+              <div class= "recruitment">
+                <h1>Recruitment</h1>
+
+              <p>You can view FAQ about recruitment and view info session dates on our <b><a id="applylink" href="apply.php">Apply Page</a></b>.</p>
+              </div>
+              <p class="citations">Information on impact investing and images taken from the <a href="https://thegiin.org/impact-investing/need-to-know/">Global Impact Investing Network</a></p>
+            </div>
+              </div>
+            <div class="sidebar">
+                <ul id="sidebarUL">
+                  <li>Who Are We?</li>
+                  <li>What is Impact Investing?</li>
+                  <li>Why Impact Investing?</li>
+                  <li>Analyst Development Program</li>
+                  <li>Recruitment</li>
+                </ul>
+                </div>
+              </div>
+          <?php include "includes/footer.php"; ?>
+        </body>
+        </html>
